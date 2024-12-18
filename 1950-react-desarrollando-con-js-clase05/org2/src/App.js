41b33516cf1bdf0d7eb138299552abcc1526ba57
ingridk12 <ingridkvegao@gmail.com>
2024-12-18 12:57:56 -0500

    enviando props

--- 1950-react-desarrollando-con-js-clase05/org2/src/App.js
+++ 1950-react-desarrollando-con-js-clase05/org2/src/App.js
@@ -10,9 +10,33 @@ function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false)
   const [colaboradores, actualizarColaboradores] = useState([{
     equipo: "Front End",
-    foto: "https://github.com/ingridk12.png",
-    nombre: "Ingrid Katherine Vega Ordoñez",
+    foto: "https://github.com/harlandlohora.png",
+    nombre: "Harland Lohora",
     puesto: "Instructor"
+  },
+  {
+    equipo: "Programación",
+    foto: "https://github.com/genesysaluralatam.png",
+    nombre: "Genesys Rondón",
+    puesto: "Desarrolladora de software e instructora"
+  },
+  {
+    equipo: "UX y Diseño",
+    foto: "https://github.com/JeanmarieAluraLatam.png",
+    nombre: "Jeanmarie Quijada",
+    puesto: "Instructora en Alura Latam"
+  },
+  {
+    equipo: "Programación",
+    foto: "https://github.com/christianpva.png",
+    nombre: "Christian Velasco",
+    puesto: "Head de Alura e Instructor"
+  },
+  {
+    equipo: "Innovación y Gestión",
+    foto: "https://github.com/JoseDarioGonzalezCha.png",
+    nombre: "Jose Gonzalez",
+    puesto: "Dev FullStack"
   }])
   //Ternario --> condicion ? seMuestra : noSeMuestra
   // condicion && seMuestra
@@ -28,7 +52,10 @@ function App() {
     //Spread operator
     actualizarColaboradores([...colaboradores, colaborador])
   }
-
+//eliminar colaborador
+const eliminarColaborador =() => {
+  console.log("Eliminar colaborador")
+}
 
   //Lista de equipos
   const equipos = [
@@ -86,6 +113,7 @@ function App() {
           datos={equipo}
           key={equipo.titulo}
           colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador ={eliminarColaborador}
         />
         )
       }
